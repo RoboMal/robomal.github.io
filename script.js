97f4bcd72c920c333040cf23cce201497a1c0939
Malcolm King <malcolm.elijah.king@gmail.com>
2024-04-13 09:57:34 -0500

    Final touchups, including styling

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ const quizData = [
         options: ["Mercury", "Pluto", "Saturn", "Earth"],
         correctAnswer: "Saturn"
     },
+    {
+        question: "In what year was vulcanised rubber discovered?",
+        options: ["1839", "1829", "1793", "1842"],
+        correctAnswer: "1839"
+    },
+    {
+        question: "Which animal's name means 'thousand feet'?",
+        options: ["Arctotherium", "Millipede", "Octopus", "Centipede"],
+        correctAnswer: "Millipede" 
+    },
+    {
+        question: "Isn't computer science fun?",
+        options: ["True", "False", "IDK"],
+        correctAnswer: "True"
+    }
   
 ];
 
